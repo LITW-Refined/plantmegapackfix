778549f30ae3dd54e47b7aec88d16298fdf3eafb
Pilzinsel64 <pilzinsel64@gmx.de>
2025-01-15 16:13:50 +0100

    try it but it doesn't work

--- src/main/java/de/pilz/plantmegapackfix/mixins/late/PMPGenVineMixin.java
+++ src/main/java/de/pilz/plantmegapackfix/mixins/late/PMPGenVineMixin.java
@@ -9,39 +9,44 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+
 import plantmegapack.block.PMPBlockPlant;
+import plantmegapack.data.PMPDataPlantSpawnParams;
 import plantmegapack.worldgen.PMPSpawnLocation;
 
 @Mixin(plantmegapack.worldgen.PMPGenVine.class)
 public abstract class PMPGenVineMixin {
 
+    @WrapOperation(
+        method = "spawnVine",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/block/Block;getMaterial()Lnet/minecraft/block/material/Material;"),
+        remap = false)
+    private static Material pmpfix$spawnVine$ensurePositiveHeight(Block block, Operation<Material> original, World world, Random random, PMPSpawnLocation locationData, PMPDataPlantSpawnParams params, PMPBlockPlant plant, @Local int height) {
+        if (locationData.y - height > 1) {
+            return original.call(block);
+        }
+        return null;
+    }
+
     @Inject(
-        method = "spawnVine(Lnet/minecraft/world/World;Ljava/util/Random;Lplantmegapack/worldgen/PMPSpawnLocation;ILplantmegapack/block/PMPBlockPlant;)Z",
-        at = @At("HEAD"),
+        method = "spawnVine",
+        at = @At(value = "INVOKE", shift = Shift.BEFORE, target = "Ljava/util/Random;nextInt(I)I"),
         cancellable = true,
         remap = false)
-    private static void spawnVine(World world, Random random, PMPSpawnLocation locationData, int metaData,
-        PMPBlockPlant plant, CallbackInfoReturnable<Boolean> clb) {
-        int height = 0;
-        int y;
-        Block block;
-
-        do {
-            ++height;
-            y = locationData.y - height;
-            block = world.getBlock(locationData.x, y, locationData.z);
-        } while (y > 1 && block.getMaterial() == Material.air);
-
-        if (y > 1) {
-            for (int actualHeight = locationData.y - random.nextInt(Math.max(1, height)); actualHeight
-                <= locationData.y; ++actualHeight) {
-                world.setBlock(locationData.x, actualHeight, locationData.z, plant, metaData, 3);
-            }
+    private static void pmpfix$spawnVine$checkHeight(World world, Random random, PMPSpawnLocation locationData, int metaData,
+        PMPBlockPlant plant, CallbackInfoReturnable<Boolean> clb, @Local int height) {
+        if (locationData.y - height <= 1) {
+            clb.setReturnValue(true);
+            clb.cancel();
         }
-
-        clb.setReturnValue(true);
-        clb.cancel();
     }
 }
